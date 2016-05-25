3ea66d52d0b93a23bb860cc61de34094d21a87d7
Yuhei Yasuda <yuhei@ryden.co.jp>
2016-05-25 16:09:18 +0900

    mod

--- scripts/deploy-to-staging.sh
+++ scripts/deploy-to-staging.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rm -rf dist/
 npm run build
-rsync -avz --delete dist/ yuhei@ssv4.ryden.co.jp:/www/test.stg2.ryden.co.jp/htdocs/
+rsync -vz --delete dist/ yuhei@ssv4.ryden.co.jp:/www/test.stg2.ryden.co.jp/htdocs/
