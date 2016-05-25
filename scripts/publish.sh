5609cfef6d961df5ff3701edfd7e6661589867cb
Yuhei Yasuda <yuhei@ryden.co.jp>
2016-05-25 17:15:40 +0900

    fix

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -5,6 +5,8 @@ find dist/. -type f | grep -v "\.git" | xargs rm
 find dist/. -type d | grep -v "\.git" | grep -v "\.$" | xargs rmdir
 npm run build
 cd dist/
+git config --global user.name "circleci"
+git config --global user.email "circleci@feedforce.jp"
 git add --all
 git commit -m "Publish"
 git push origin publish
