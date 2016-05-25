c5f2b35a393b1bd171d46f3530e81364b393131b
Yuhei Yasuda <yuhei@ryden.co.jp>
2016-05-25 17:06:02 +0900

    fix

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm -rf dist/
-git clone -b publish xxxxxxxx dist
+git clone -b publish https://github.com/yuheiy-sandbox/internal-ci.git dist
 find dist/. -type f | grep -v "\.git" | xargs rm
 find dist/. -type d | grep -v "\.git" | grep -v "\.$" | xargs rmdir
 npm run build
